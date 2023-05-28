86d82f5c6b8596e7234d60867648b3bf6f99c139
shadyelnady <shadyelnady@gmail.com>
2023-05-28 19:50:24 +0300

    first commit

--- API/management/commands/seeds.py
+++ API/management/commands/seeds.py
@@ -5,7 +5,9 @@ import random
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.conf import settings
+from Restaurant.models import UserRestaurant
 
+from User.models import Profile, User
 
 
 class Command(BaseCommand):
@@ -15,7 +17,7 @@ class Command(BaseCommand):
         works = [
             "makemigrations",
             "migrate",
-            "createsuperuser",
+            # "createsuperuser",
             "collectstatic",
         ]
         for work in works:
@@ -39,7 +41,7 @@ class Command(BaseCommand):
             "categories.json",
             "meals.json",
             "drivers.json",
-            # "users.json",
+            "users.json",
             "hospitalities.json",
             "colors.json",
             "gradients.json",
@@ -59,6 +61,35 @@ class Command(BaseCommand):
             except Exception as e:
                 self.stdout.write(
                     self.style.ERROR(f"Load Data Failed from {data} , \n \t Error is: \t \t{e}")
+                )
+        ##
+
+        User.objects.create_superuser(
+            username= "Shady",
+            email= "shadyelnady@gmail.com",
+            password= "12345",
+        )
+        User.objects.create_user(
+            username= "M",
+            email= "m@gmail.com",
+            password= "12345",
+        )
+        ##
+        for index, profile in enumerate(Profile.objects.all().order_by("created_at")):
+            try:
+                profile.image = f"images/Profile/{index}.png"
+                profile.save()
+                UserRestaurant(
+                    user = profile.user,
+                    restaurant = 1,
+                    is_favorite = True,
+                    comment = "Food, as always, is good both upstairs and downstairs is always clean (download the bk app for deals etc.) sit upstairs every time, more relaxed feel.",
+                    review = 4.5,
+                    likes = 4,
+                ).save()
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR(f"Error in Profile > {e}")
                 )
         
         self.stdout.write(
